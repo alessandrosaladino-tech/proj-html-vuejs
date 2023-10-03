f990214554f9a84ce206791a6bc05881651d886a
alessandrosaladino-tech <alessandro.saladino22@gmail.com>
2023-10-03 11:39:37 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<script >
+export default {
+  data(){
+    return {
+
+    }
+  }
+}
+</script>
+
+<template>
+
+<section>
+  <header>
+    <div class="container">
+      <div class="header_top"></div>
+    </div>
+  </header>
+</section>
+
+
+
+
+
+
+
+
+
+
+</template>
+
+<style scoped>
+
+</style>
