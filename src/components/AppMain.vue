4a93027780a8c3e81349a140276fd554a2e3e9b0
alessandrosaladino-tech <alessandro.saladino22@gmail.com>
2023-10-05 12:08:35 +0200

    Add
    AppNews

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -4,6 +4,7 @@ import AppBanner from './AppBanner.vue';
 import AppCourse from './AppCourses.vue';
 import AppFirstStep from './AppFirstStep.vue';
 import AppEvents from './AppEvents.vue';
+import AppNews from './AppNews.vue';
 export default {
     name: "AppMain",
     components: {
@@ -12,6 +13,7 @@ export default {
         AppCourse,
         AppFirstStep,
         AppEvents,
+        AppNews,
     }
 }
 </script>
@@ -23,6 +25,7 @@ export default {
         <AppCourse></AppCourse>
         <AppFirstStep></AppFirstStep>
         <AppEvents></AppEvents>
+        <AppNews></AppNews>
     </main>
 </template>
 
---
+++ src/components/AppNews.vue
@@ -0,0 +1,67 @@
+<script>
+export default {
+    name: "AppNews",
+    data() {
+        return {
+            news: [
+                {
+                    img: "../src/assets/img/mt-2236-blog-img1.jpg",
+                    bio: "The most common mistakes when managing personal finances",
+                },
+                {
+                    img: "../src/assets/img/mt-2236-blog-img2.jpg",
+                    bio: "Methods of the recruitment",
+                },
+            ]
+        }
+    }
+}
+</script>
+
+<template>
+    <section class="news">
+        <div class="container d-flex news_container">
+            <div class=" d-flex gap-4">
+                <div class="col-4">
+                    <div class="box">
+                        <h1 class="fw-bold ">Latest News</h1>
+                        <div class="border_news"></div>
+                    </div>
+
+                    <button type="button" class="btn btn-outline-dark fw-bold">View Other P osts</button>
+                </div>
+                <div class="col " v-for="item in news">
+                    <img :src="item.img" alt="">
+                    <p class="pt-3">{{ item.bio }}</p>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+
+
+<style lang="scss" scoped>
+@use "../assets/scss/partials/variables" as *;
+
+.news {
+    background-color: #f8f7f7;
+
+    & .news_container {
+        padding-top: 7rem;
+        padding-bottom: 5rem;
+    }
+
+    & .border_news {
+        border-bottom: 2px solid $chess_primary;
+        width: 20%;
+        padding-top: 2rem;
+        margin-bottom: 2.8rem;
+    }
+}
+
+
+.col4,
+img {
+    width: 100%;
+}
+</style>
